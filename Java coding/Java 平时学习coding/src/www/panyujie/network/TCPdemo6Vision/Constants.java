969c3c420cee5917ad792a1bd8a3b7ee5cfcdde8
Pan_Yujie <646836760@qq.com>
2022-02-20 20:12:52 +0800

    删除

--- Java coding/Java 平时学习coding/src/www/panyujie/network/TCPdemo6Vision/Constants.java
+++
@@ -1,9 +0,0 @@
-package www.panyujie.network.TCPdemo6Vision;
-
-public class Constants {
-	/** 常量 */
-	public static final int PORT = 7778 ;
-	
-	/** 协议分隔符 */
-	public static final String SPILIT = "003197♣♣㏘♣④④♣";
-}
